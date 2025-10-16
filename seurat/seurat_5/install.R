8ec3baf42eb20288c4fa3f379348e5db0a0d6ae2
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-10-16 09:33:48 -0400

    fix: upgrade="never"

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 args <- commandArgs(trailingOnly = TRUE)
 setRepositories(ind = 1:3)
-remotes::install_github("satijalab/seurat-wrappers")
-remotes::install_github("chris-mcginnis-ucsf/DoubletFinder")
-remotes::install_github("mdelacre/Routliers")
-remotes::install_cran("BPCells", repos = c("https://bnprks.r-universe.dev"))
+remotes::install_github("satijalab/seurat-wrappers", upgrade = "never")
+remotes::install_github("chris-mcginnis-ucsf/DoubletFinder", upgrade = "never")
+remotes::install_version("mdelacre/Routliers", version = "0.0.0.3", upgrade = "never")
+remotes::install_cran("BPCells", repos = c("https://bnprks.r-universe.dev"), upgrade = "never")
