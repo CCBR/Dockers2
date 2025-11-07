4d86a6e5517257ea701802d90793323fede31056
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-11-07 16:05:49 -0500

    fix: throw error if packages are not installed

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 args <- commandArgs(trailingOnly = TRUE)
 setRepositories(ind = 1:3)
-remotes::install_github("satijalab/seurat-wrappers", upgrade = "never")
+remotes::install_github("satijalab/seurat-wrappers@73466e3", upgrade = "never")
 
 # https://github.com/chris-mcginnis-ucsf/DoubletFinder/issues/244
 remotes::install_github("chris-mcginnis-ucsf/DoubletFinder@67fb8b5", upgrade = "never")
@@ -9,3 +9,17 @@ remotes::install_github("chris-mcginnis-ucsf/DoubletFinder@67fb8b5", upgrade = "
 remotes::install_version("Routliers", repos = "https://cran.rstudio.com/", version = "0.0.0.3", upgrade = "never")
 remotes::install_cran("BPCells", repos = "https://bnprks.r-universe.dev", upgrade = "never")
 remotes::install_cran("gypsum", repos = "https://bioc.r-universe.dev/", upgrade = "never")
+
+abort_packages_not_installed <- function(...) {
+  package_status <- lapply(c(...), rlang::is_installed)
+  packages_not_installed <- Filter(isFALSE, package_status) |> unlist()
+  if (length(packages_not_installed) > 0) {
+    msg <- paste0(
+      "The following package(s) are required but are not installed: \n  ",
+      paste0(names(packages_not_installed), collapse = ", ")
+    )
+    stop(msg)
+  }
+}
+
+abort_packages_not_installed('SeuratWrappers', 'DoubletFinder', 'Routliers', 'BPCells', 'gypsum')
