1946e9f85ec75e857d3b196a2ea898a352333a0a
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-11-07 15:25:52 -0500

    fix: set DoubletFinder commit hash from R lib dir on biowulf

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -4,7 +4,7 @@ setRepositories(ind = 1:3)
 remotes::install_github("satijalab/seurat-wrappers", upgrade = "never")
 
 # https://github.com/chris-mcginnis-ucsf/DoubletFinder/issues/244
-remotes::install_github("chris-mcginnis-ucsf/DoubletFinder@3b420df", upgrade = "never")
+remotes::install_github("chris-mcginnis-ucsf/DoubletFinder@67fb8b5", upgrade = "never")
 
 remotes::install_version("Routliers", repos = "https://cran.rstudio.com/", version = "0.0.0.3", upgrade = "never")
 remotes::install_cran("BPCells", repos = "https://bnprks.r-universe.dev", upgrade = "never")
