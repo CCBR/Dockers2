465206d72c6f0016d1d3c955cce47e6dcb08c611
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-11-21 10:57:30 -0500

    fix: install gypsum before celldex

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -2,6 +2,7 @@
 args <- commandArgs(trailingOnly = TRUE)
 setRepositories(ind = 1:3)
 
+remotes::install_cran("gypsum", repos = "https://bioc.r-universe.dev/", upgrade = "never")
 remotes::install_bioc("celldex", upgrade = "never")
 
 remotes::install_github("satijalab/seurat-wrappers@73466e3", upgrade = "never")
@@ -11,7 +12,6 @@ remotes::install_github("chris-mcginnis-ucsf/DoubletFinder@3b420df", upgrade = "
 
 remotes::install_version("Routliers", repos = "https://cran.rstudio.com/", version = "0.0.0.3", upgrade = "never")
 remotes::install_cran("BPCells", repos = "https://bnprks.r-universe.dev", upgrade = "never")
-remotes::install_cran("gypsum", repos = "https://bioc.r-universe.dev/", upgrade = "never")
 
 abort_packages_not_installed <- function(...) {
   package_status <- lapply(c(...), rlang::is_installed)
@@ -25,4 +25,4 @@ abort_packages_not_installed <- function(...) {
   }
 }
 
-abort_packages_not_installed('SeuratWrappers', 'DoubletFinder', 'Routliers', 'BPCells', 'gypsum')
+abort_packages_not_installed('SeuratWrappers', 'DoubletFinder', 'Routliers', 'BPCells', 'gypsum', 'celldex')
