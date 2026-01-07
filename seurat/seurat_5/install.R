587033b1015505b752c02993e990ede902436127
Kelly Sovacool <kelly.sovacool@nih.gov>
2026-01-07 10:32:54 -0500

    style: auto format

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -2,16 +2,32 @@
 args <- commandArgs(trailingOnly = TRUE)
 setRepositories(ind = 1:3)
 
-remotes::install_cran("gypsum", repos = "https://bioc.r-universe.dev/", upgrade = "never")
+remotes::install_cran(
+  "gypsum",
+  repos = "https://bioc.r-universe.dev/",
+  upgrade = "never"
+)
 remotes::install_bioc("celldex", upgrade = "never")
 
 remotes::install_github("satijalab/seurat-wrappers@73466e3", upgrade = "never")
 
 # https://github.com/chris-mcginnis-ucsf/DoubletFinder/issues/244
-remotes::install_github("chris-mcginnis-ucsf/DoubletFinder@3b420df", upgrade = "never")
+remotes::install_github(
+  "chris-mcginnis-ucsf/DoubletFinder@3b420df",
+  upgrade = "never"
+)
 
-remotes::install_version("Routliers", repos = "https://cran.rstudio.com/", version = "0.0.0.3", upgrade = "never")
-remotes::install_cran("BPCells", repos = "https://bnprks.r-universe.dev", upgrade = "never")
+remotes::install_version(
+  "Routliers",
+  repos = "https://cran.rstudio.com/",
+  version = "0.0.0.3",
+  upgrade = "never"
+)
+remotes::install_cran(
+  "BPCells",
+  repos = "https://bnprks.r-universe.dev",
+  upgrade = "never"
+)
 
 abort_packages_not_installed <- function(...) {
   pkgs <- c(...)
@@ -20,11 +36,18 @@ abort_packages_not_installed <- function(...) {
   packages_not_installed <- Filter(isFALSE, package_status)
   if (length(packages_not_installed) > 0) {
     msg <- paste0(
-      "The following package(s) are required but are not installed: \n  ",
+      "The following package(s) are required but are not installed: ",
       paste0(names(packages_not_installed), collapse = ", ")
     )
     stop(msg)
   }
 }
 
-abort_packages_not_installed('SeuratWrappers', 'DoubletFinder', 'Routliers', 'BPCells', 'gypsum', 'celldex')
+abort_packages_not_installed(
+  'SeuratWrappers',
+  'DoubletFinder',
+  'Routliers',
+  'BPCells',
+  'gypsum',
+  'celldex'
+)
