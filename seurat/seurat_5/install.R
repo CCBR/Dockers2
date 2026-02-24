71011f20368a5dff3765de3c823d21d3661df352
Nathan Wong <nathan.wong@nih.gov>
2026-02-24 15:47:24 -0500

    test: Rv4.4.3 with primary installations in environment.yml

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -7,10 +7,10 @@ remotes::install_cran(
   repos = "https://bioc.r-universe.dev/",
   upgrade = "never"
 )
-remotes::install_bioc("rdfh5", upgrade = "never") 
-remotes::install_bioc("assorthead", upgrade = "never")
-remotes::install_bioc("alabaster.base", upgrade = "never")
-remotes::install_bioc("celldex", upgrade = "never")
+#remotes::install_bioc("rhdf5", upgrade = "never") 
+#remotes::install_bioc("assorthead", upgrade = "never")
+#remotes::install_bioc("alabaster.base", upgrade = "never")
+#remotes::install_bioc("celldex", upgrade = "never")
 
 remotes::install_github("satijalab/seurat-wrappers@73466e3", upgrade = "never")
 
