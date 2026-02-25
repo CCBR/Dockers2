73ed8a7cb3057941301cc7bc991322c5955a3be9
Nathan Wong <nathan.wong@nih.gov>
2026-02-25 10:00:44 -0500

    fix: install rliger via install.R

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -7,6 +7,7 @@ remotes::install_cran(
   repos = "https://bioc.r-universe.dev/",
   upgrade = "never"
 )
+remotes::install_cran("rliger", upgrade = "never")
 #remotes::install_bioc("rhdf5", upgrade = "never") 
 #remotes::install_bioc("assorthead", upgrade = "never")
 #remotes::install_bioc("alabaster.base", upgrade = "never")
