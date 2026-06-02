4cf758c8492601dd73e4ca94b1a6444ed0c262b1
Vishal Koparde <vishal.koparde@nih.gov>
2026-06-02 00:18:36 +0000

    feat: new versions of rose

--- rose/push.sh
+++ rose/push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-REPONAME="ccbr_rose""
-BUILD_TAG="v2"
+REPONAME="ccbr_rose"
+BUILD_TAG="v3"
 docker push nciccbr/${REPONAME}:${BUILD_TAG} 
 docker tag nciccbr/${REPONAME}:${BUILD_TAG} nciccbr/${REPONAME}:latest
 docker push nciccbr/${REPONAME}:latest
