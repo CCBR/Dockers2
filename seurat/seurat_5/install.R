e9ad28265af6a6e250e56623809c871b8b80976a
Nathan W <38703762+wong-nw@users.noreply.github.com>
2026-07-23 11:28:31 -0400

    Apply suggestions from code review
    Co-authored-by: Kelly Sovacool, PhD <kelly-sovacool@users.noreply.github.com>

--- seurat/seurat_5/install.R
+++ seurat/seurat_5/install.R
@@ -33,10 +33,13 @@ remotes::install_cran(
   upgrade = "never"
 )
 
+# install scot w/ imports + suggests
 remotes::install_github(
-  "CCBR/SCOT", ref = "main"
+  "CCBR/SCOT", ref = "main", dependencies = TRUE
 )
 
+# make sure scot was installed properly
+library(SCOT)
 abort_packages_not_installed <- function(...) {
   pkgs <- c(...)
   package_status <- lapply(pkgs, rlang::is_installed) |> unlist()
@@ -59,5 +62,6 @@ abort_packages_not_installed(
   'gypsum',
   'alabaster.base',
   'celldex',
-  'rliger'
+  'rliger',
+  'SCOT'
 )
